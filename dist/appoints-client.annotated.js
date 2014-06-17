7b5da6499b2f52c5b91c5be1170c25fc7db6ba50
Martijn Boland <martijn@boland.org>
2014-06-17 14:35:13 +0200

    Initial version, build infrastructure (gulp, bower etc)

---
+++ dist/appoints-client.annotated.js
@@ -0,0 +1,4 @@
+angular.module('appointsClient', [
+  'ngRoute',
+  'appoints-client-templates'
+]);
---
+++ dist/appoints-client.js
@@ -0,0 +1,5 @@
+
+angular.module('appointsClient', [
+  'ngRoute',
+  'appoints-client-templates'
+]);
